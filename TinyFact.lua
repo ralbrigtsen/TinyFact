cf73315c5e20cb55425c49ba82363fb6a3495364
ZnatNO <dawgie@gmail.com>
2016-09-02 03:32:45 +0200

    Change printout
    Changed the printout so that it'll notify you if you have your next
    trait available.

--- TinyFact.lua
+++ TinyFact.lua
@@ -1,16 +1,20 @@
 -- TinyFact by Znat
 -- local TinyFact = CreateFrame("frame","TinyFact","UIParent")
 
-print('TinyFact v0.00001b loaded!')
+print('TinyFact v0.1 loaded!')
 
 local TinyFact_EventFrame = CreateFrame("Frame")
 
 SLASH_TINYFACT1, SLASH_TINYFACT2 = '/tf', '/tinyfact'; 
 function SlashCmdList.TINYFACT(msg, editbox) 
-  local TinyFactRank = select(6,C_ArtifactUI.GetEquippedArtifactInfo())
-  local TinyFactPoints = select(5,C_ArtifactUI.GetEquippedArtifactInfo())
-  local TinyFactNeededPoints = C_ArtifactUI.GetCostForPointAtRank(select(6,C_ArtifactUI.GetEquippedArtifactInfo()))
-  print('Artifact is level ' .. TinyFactRank ..', You got ' .. TinyFactPoints ..'/'.. TinyFactNeededPoints ..' for the next level.' )
+	local TinyFactRank = select(6,C_ArtifactUI.GetEquippedArtifactInfo())
+	local TinyFactPoints = select(5,C_ArtifactUI.GetEquippedArtifactInfo())
+	local TinyFactNeededPoints = C_ArtifactUI.GetCostForPointAtRank(select(6,C_ArtifactUI.GetEquippedArtifactInfo()))
+	if TinyFactPoints < TinyFactNeededPoints then
+		print('Artifact: L\124c1188FF88' .. TinyFactRank ..'\124r, Points \124c11FF8888' .. TinyFactPoints ..'\124r/\124c1188FF88' .. TinyFactNeededPoints .. '\124r for the next trait.')
+	elseif TinyFactPoints >= TinyFactNeededPoints then
+		print('Artifact: L\124c1188FF88' .. TinyFactRank ..'\124r, Points \124c1188FF88' .. TinyFactPoints ..'\124r/\124c1188FF88' .. TinyFactNeededPoints .. '\124r. New \124c11FF8888trait\124r available!')
+	end
 end
 
 TinyFact_EventFrame:RegisterEvent("ARTIFACT_XP_UPDATE")
@@ -19,5 +23,9 @@ TinyFact_EventFrame:SetScript("OnEvent",
 		local TinyFactRank = select(6,C_ArtifactUI.GetEquippedArtifactInfo())
 		local TinyFactPoints = select(5,C_ArtifactUI.GetEquippedArtifactInfo())
 		local TinyFactNeededPoints = C_ArtifactUI.GetCostForPointAtRank(select(6,C_ArtifactUI.GetEquippedArtifactInfo()))
-		print('Artifact: L' .. TinyFactRank ..', Points ' .. TinyFactPoints ..'/'.. TinyFactNeededPoints ..' for the next level.' )
+		if TinyFactPoints < TinyFactNeededPoints then
+			print('Artifact: L\124c1188FF88' .. TinyFactRank ..'\124r, Points \124c11FF8888' .. TinyFactPoints ..'\124r/\124c1188FF88' .. TinyFactNeededPoints .. '\124r for the next trait.')
+		elseif TinyFactPoints >= TinyFactNeededPoints then
+			print('Artifact: L\124c1188FF88' .. TinyFactRank ..'\124r, Points \124c1188FF88' .. TinyFactPoints ..'\124r/\124c1188FF88' .. TinyFactNeededPoints .. '\124r. New \124c11FF8888trait\124r available!')
+		end
 end)
