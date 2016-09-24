91eed8b7ba81f8715075963aa26dc66101381b6a
ZnatNO <dawgie@gmail.com>
2016-09-24 02:37:45 +0200

    Perk-revamp
    Steino reworked forgeframe.

--- TinyFact.lua
+++ TinyFact.lua
@@ -1,5 +1,5 @@
 -- TinyFact by Znat
--- Version 1.1 
+-- Version 1.2
 -- Changes: Added needed xp to forgeframe.
 -- Codeninja from Steinop@Stormscale
 
@@ -22,43 +22,34 @@ end
 TinyFact_EventFrame:RegisterEvent("ARTIFACT_XP_UPDATE")
 TinyFact_EventFrame:RegisterEvent("ADDON_LOADED")
 
-TinyFact_EventFrame:SetScript("OnEvent",
-        function(self, event, ...)
-            if event == "ARTIFACT_XP_UPDATE" then
-            if ArtifactFrame.PerksTab.TitleContainer:IsShown() then
-                local _, _, _, _, totalXP, pointsSpent = C_ArtifactUI.GetEquippedArtifactInfo();
-                local _,xp, xpForNextPoint = MainMenuBar_GetNumArtifactTraitsPurchasableFromXP(pointsSpent, totalXP);
-                if xpForNextPoint > 0 then
-                   ArtifactFrame.PerksTab.TitleContainer.PointsRemainingLabel:SetText(xp .. " \124c11FF8888(+" .. xpForNextPoint-xp ..")\124r");
-                elseif xpForNextPoint < 0 then
-                   ArtifactFrame.PerksTab.TitleContainer.PointsRemainingLabel:SetText(xp .. " \124c1188FF88(Upgrade!)\124r");
-                end
-            end
-                local TinyFactRank = select(6,C_ArtifactUI.GetEquippedArtifactInfo())
-                local TinyFactPoints = select(5,C_ArtifactUI.GetEquippedArtifactInfo())
-                local TinyFactNeededPoints = C_ArtifactUI.GetCostForPointAtRank(select(6,C_ArtifactUI.GetEquippedArtifactInfo()))
-                if TinyFactPoints < TinyFactNeededPoints then
-                        print('Artifact: L\124c1188FF88' .. TinyFactRank ..'\124r, Points \124c11FF8888' .. TinyFactPoints ..'\124r/\124c1188FF88' .. TinyFactNeededPoints .. '\124r for the next trait.')
-                elseif TinyFactPoints >= TinyFactNeededPoints then
-                        print('Artifact: L\124c1188FF88' .. TinyFactRank ..'\124r, Points \124c1188FF88' .. TinyFactPoints ..'\124r/\124c1188FF88' .. TinyFactNeededPoints .. '\124r. New \124c11FF8888trait\124r available!')
-                end
-            end
-    if event == "ADDON_LOADED" then
+local TinyFactPerk = CreateFrame"Frame"
+
+TinyFactPerk:Hide()
+TinyFactPerk:RegisterEvent"ARTIFACT_XP_UPDATE"
+TinyFactPerk:RegisterEvent"ARTIFACT_UPDATE"
+TinyFactPerk:RegisterEvent"ADDON_LOADED"
+TinyFactPerk:SetScript("OnEvent", function(self, event, ...)
+      if event == "ARTIFACT_XP_UPDATE" then
+         if ArtifactFrame.PerksTab.TitleContainer:IsShown() then
+            local _, _, _, _, totalXP, pointsSpent = C_ArtifactUI.GetArtifactInfo()
+            local _, xp, xpForNextPoint = MainMenuBar_GetNumArtifactTraitsPurchasableFromXP(pointsSpent, totalXP);
+            
+            ArtifactFrame.PerksTab.TitleContainer.PointsRemainingLabel:SetText(xp .. " \124c11FF8888(+" .. xpForNextPoint-xp ..")\124r")
+         end
+      end
+      if event == "ADDON_LOADED" then
          local addon = ...
          if addon == "Blizzard_ArtifactUI" then
             self:UnregisterEvent"ADDON_LOADED"
-            print"Hook that shit"
-            ArtifactFrame.PerksTab.TitleContainer:HookScript("OnShow",
-        function (self, event, ...)
-                local _, _, _, _, totalXP, pointsSpent = C_ArtifactUI.GetEquippedArtifactInfo();
-                local _,xp, xpForNextPoint = MainMenuBar_GetNumArtifactTraitsPurchasableFromXP(pointsSpent, totalXP);
-                if xpForNextPoint > 0 then
-                   ArtifactFrame.PerksTab.TitleContainer.PointsRemainingLabel:SetText(xp .. " \124c11FF8888(+" .. xpForNextPoint-xp ..")\124r");
-                elseif xpForNextPoint < 0 then
-                   ArtifactFrame.PerksTab.TitleContainer.PointsRemainingLabel:SetText(xp .. " \124c1188FF88(Upgrade!)\124r");
-                end
-end)
+            hooksecurefunc(ArtifactFrame.PerksTab.TitleContainer.PointsRemainingLabel,"SetText",function()
+               local _, _, _, _, totalXP, pointsSpent = C_ArtifactUI.GetArtifactInfo()
+               local _, xp, xpForNextPoint = MainMenuBar_GetNumArtifactTraitsPurchasableFromXP(pointsSpent, totalXP)
+               ArtifactFrame.PerksTab.TitleContainer.PointsRemainingLabel:SetText(xp .. " \124c11FF8888(+" .. xpForNextPoint-xp ..")\124r")
+            end)
             ArtifactFrame.PerksTab.TitleContainer:SetScript("OnUpdate", nil)
+               local _, _, _, _, totalXP, pointsSpent = C_ArtifactUI.GetArtifactInfo()
+               local _, xp, xpForNextPoint = MainMenuBar_GetNumArtifactTraitsPurchasableFromXP(pointsSpent, totalXP)
+               ArtifactFrame.PerksTab.TitleContainer.PointsRemainingLabel:SetText(xp .. " \124c11FF8888(+" .. xpForNextPoint-xp ..")\124r")
          end
       end
 end)
