f9dcb0a87b36461777fc83f1e7c895bce41868f7
ZnatNO <dawgie@gmail.com>
2016-10-05 18:57:00 +0200

    Next version
    Didn't check if artifactframe exists, now does.

--- TinyFact.lua
+++ TinyFact.lua
@@ -1,9 +1,9 @@
--- TinyFact by Znat
+-- TinyFact by Znat@Stormscale (EU)
 -- Version 1.3
 -- Changes: Added needed xp to forgeframe.
--- Codeninja from Steinop@Stormscale
+-- Codeninja from Steinp@Stormscale
 
-print('TinyFact v1.3.2 loaded!')
+print('TinyFact v1.3.3 loaded!')
 
 local TinyFact_EventFrame = CreateFrame("Frame")
 
@@ -40,7 +40,7 @@ TinyFactPerk:RegisterEvent"ARTIFACT_UPDATE"
 TinyFactPerk:RegisterEvent"ADDON_LOADED"
 TinyFactPerk:SetScript("OnEvent", function(self, event, ...)
       if event == "ARTIFACT_XP_UPDATE" then
-         if ArtifactFrame.PerksTab.TitleContainer:IsShown() then
+         if ArtifactFrame and ArtifactFrame.PerksTab.TitleContainer:IsShown() then
             local _, _, _, _, totalXP, pointsSpent = C_ArtifactUI.GetArtifactInfo()
             local _, xp, xpForNextPoint = MainMenuBar_GetNumArtifactTraitsPurchasableFromXP(pointsSpent, totalXP);
             
