a16eddf4bc3a72e742c58ad63dd3aa8972b838a3
ZnatNO <dawgie@gmail.com>
2017-04-15 15:19:55 +0200

    changed numberformatting
    Changed numberformatting on artifact-points, now humanreadable.

--- TinyFact.lua
+++ TinyFact.lua
@@ -1,12 +1,30 @@
 -- TinyFact by Znat@Stormscale (EU) & Steinp@Stormscale
--- Version 1.3.5
+-- Version 1.3.6
 -- www.mindfields.no
 
 
-print('TinyFact v1.3.5 loaded!')
+print('TinyFact v1.3.6 loaded!')
 
 local TinyFact_EventFrame = CreateFrame("Frame")
 local orig_SetText
+
+local function ReadableNumber(num, places)
+    local ret
+    -- local placeValue = ("%%.%df"):format(places or 0)
+    local placeValue = ("%%.1f"):format(places or 0)
+    if not num then
+        return 0
+    elseif num >= 2000000000 then
+        ret = placeValue:format(num / 1000000000) .. "bn" -- billion
+    elseif num >= 2000000 then
+        ret = placeValue:format(num / 1000000) .. "m" -- million
+    elseif num >= 200000 then
+        ret = placeValue:format(num / 1000) .. "k" -- thousand
+    else
+        ret = BreakUpLargeNumbers(num) -- hundreds
+    end
+    return ret
+end
 
 SLASH_TINYFACT1, SLASH_TINYFACT2 = '/tf', '/tinyfact'; 
 function SlashCmdList.TINYFACT(msg, editbox) 
@@ -15,9 +33,9 @@ function SlashCmdList.TINYFACT(msg, editbox)
         local TinyFactPoints = select(5,C_ArtifactUI.GetEquippedArtifactInfo())
         local TinyFactNeededPoints = C_ArtifactUI.GetCostForPointAtRank(TinyFactRank,TinyFactTier)
         if TinyFactPoints < TinyFactNeededPoints then
-                print('Artifact: L\124c1188FF88' .. TinyFactRank ..'\124r, Points \124c11FF8888' .. TinyFactPoints ..'\124r/\124c1188FF88' .. TinyFactNeededPoints .. '\124r for the next trait.')
+                print('Artifact: L\124c1188FF88' .. TinyFactRank ..'\124r, Points \124c11FF8888' .. ReadableNumber(TinyFactPoints) ..'\124r/\124c1188FF88' .. ReadableNumber(TinyFactNeededPoints) .. '\124r for the next trait.')
         elseif TinyFactPoints >= TinyFactNeededPoints then
-                print('Artifact: L\124c1188FF88' .. TinyFactRank ..'\124r, Points \124c1188FF88' .. TinyFactPoints ..'\124r/\124c1188FF88' .. TinyFactNeededPoints .. '\124r. New \124c11FF8888trait\124r available!')
+                print('Artifact: L\124c1188FF88' .. TinyFactRank ..'\124r, Points \124c1188FF88' .. ReadableNumber(TinyFactPoints) ..'\124r/\124c1188FF88' .. ReadableNumber(TinyFactNeededPoints) .. '\124r. New \124c11FF8888trait\124r available!')
         end
 end
 
@@ -29,9 +47,9 @@ TinyFact_EventFrame:SetScript("OnEvent",
     local TinyFactPoints = select(5,C_ArtifactUI.GetEquippedArtifactInfo())
     local TinyFactNeededPoints = C_ArtifactUI.GetCostForPointAtRank(TinyFactRank,TinyFactTier)
     if TinyFactPoints < TinyFactNeededPoints then
-      print('Artifact: L\124c1188FF88' .. TinyFactRank ..'\124r, Points \124c11FF8888' .. TinyFactPoints ..'\124r/\124c1188FF88' .. TinyFactNeededPoints .. '\124r for the next trait.')
+      print('Artifact: L\124c1188FF88' .. TinyFactRank ..'\124r, Points \124c11FF8888' .. ReadableNumber(TinyFactPoints) ..'\124r/\124c1188FF88' .. ReadableNumber(TinyFactNeededPoints) .. '\124r for the next trait.')
     elseif TinyFactPoints >= TinyFactNeededPoints then
-      print('Artifact: L\124c1188FF88' .. TinyFactRank ..'\124r, Points \124c1188FF88' .. TinyFactPoints ..'\124r/\124c1188FF88' .. TinyFactNeededPoints .. '\124r. New \124c11FF8888trait\124r available!')
+      print('Artifact: L\124c1188FF88' .. TinyFactRank ..'\124r, Points \124c1188FF88' .. ReadableNumber(TinyFactPoints) ..'\124r/\124c1188FF88' .. ReadableNumber(TinyFactNeededPoints) .. '\124r. New \124c11FF8888trait\124r available!')
     end
 end)
 
@@ -56,8 +74,8 @@ function _SetText()
   local _, _, _, _, totalXP, pointsSpent, _, _, _, _, _, _, tier = C_ArtifactUI.GetArtifactInfo()
   local numPoints, xp, xpForNextPoint = MainMenuBar_GetNumArtifactTraitsPurchasableFromXP(pointsSpent, totalXP, tier)
   if numPoints > 0 then
-    SetText(ArtifactFrame.PerksTab.TitleContainer.PointsRemainingLabel, xp .. " \124c1188FF88(+" .. xpForNextPoint-xp ..") +" .. numPoints .."\124r")
+    SetText(ArtifactFrame.PerksTab.TitleContainer.PointsRemainingLabel, ReadableNumber(xp) .. " \124c1188FF88(+" .. ReadableNumber(xpForNextPoint-xp) ..") +" .. numPoints .."\124r")
   else
-    SetText(ArtifactFrame.PerksTab.TitleContainer.PointsRemainingLabel, xp .. " \124c11FF8888(+" .. xpForNextPoint-xp ..")\124r")
+    SetText(ArtifactFrame.PerksTab.TitleContainer.PointsRemainingLabel, ReadableNumber(xp) .. " \124c11FF8888(+" .. ReadableNumber(xpForNextPoint-xp) ..")\124r")
   end
 end
